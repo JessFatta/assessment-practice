464c5fee44ccafa15db7ec801b249a9a71d3b85f
Jessica Fatta <jessicafatta@gmail.com>
2021-09-22 17:42:01 -0400

    initial commit

---
+++ practice.js
@@ -0,0 +1,9 @@
+// assessment practice messing around
+
+var breakfast = {
+  eggs: "sunny side up",
+  drink: "coffee",
+  isSatisfied: true,
+  numberOfEggs: 2,
+  toDoList: ["study for assessment", "drink water"],
+};
